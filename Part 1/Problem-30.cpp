2f70d06a56090a7491c2789bff83c1cf12c30cd0
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-01-25 10:59:24 +0600

    Added new solution.

---
+++ Part 1/Problem-30.cpp
@@ -0,0 +1,27 @@
+// Your task is to find out how many times letter 'A' can be found in given sequence.
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int size, i, times = 0;
+	char y[] = "A";
+
+	cin >> size;
+	char x[size];
+
+	if (size >= 1 && size <= 1000) {
+
+		for (i = 0; i <= (size - 1); i++)
+			cin >> x[i];
+
+		for (i = (size - 1); i >= 0; i--)
+			if (x[i] == y[0]) times++;
+		
+		cout << times;
+	}
+
+	return 0;
+}
