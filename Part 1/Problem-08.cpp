607cb9b4ad2321b36cb163b0d32ff463b50d3a74
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-01-25 10:07:03 +0600

    Added new solution.

---
+++ Part 1/Problem-08.cpp
@@ -0,0 +1,12 @@
+// Input two numbers and show the remainder from the division of the first number by the second one.
+
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a % b;
+}
