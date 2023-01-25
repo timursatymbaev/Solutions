789fbe863a5913badea1bd226bd29edd58d82d7e
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-01-25 10:54:36 +0600

    Added new solution.

---
+++ Part 1/Problem-17.cpp
@@ -0,0 +1,17 @@
+// Given a year number. Output the number of days of that year.
+
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+	int x;
+
+	cin >> x;
+
+	if (((x % 100 != 0) && (x % 4 == 0)) || (x % 400 == 0)) cout << 366;
+	else cout << 365;
+
+	return 0;
+}
