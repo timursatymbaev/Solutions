4f5b07abcaccbb9087ec284b9aaf1e8482d35edf
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-01-25 10:56:44 +0600

    Added new solution.

---
+++ Part 1/Problem-23.cpp
@@ -0,0 +1,23 @@
+// Given real number a, natural number n. Calculate: an. Do not use multiplication.
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main () 
+{
+	int m;
+	double a, n;
+
+	cin >> a >> n;
+	m = int(n);
+	n = 0;
+
+	for (int i = 1; i <= m; i++)
+		n = a + n;
+
+	cout << n;
+
+	return 0;
+}
