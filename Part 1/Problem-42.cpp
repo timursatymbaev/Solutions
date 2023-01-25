5557950202f1c6df88a9ac538bd2a212058e6780
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-01-26 00:17:02 +0600

    Added new solution.

---
+++ Part 1/Problem-42.cpp
@@ -0,0 +1,34 @@
+/*
+  Sum under main diagonal.
+
+  Calculate the total sum of element that are located under main diagonal in a given NxN square table.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int size, i = 0, n = 0, m = 1, res = 0;
+
+	cin >> size;
+
+	if (size >= 1 && size <= 100) {
+		int x[size][size];
+
+		for (i = 0; i < size; i++)
+			for (n = 0; n < size; n++)
+				cin >> x[i][n];
+
+		for (i = 1; i < size; i++) {
+			for (n = 0; n < m; n++)
+				res += x[i][n];
+			m++;
+		}
+
+		cout << res;
+	}
+
+	return 0;
+}
