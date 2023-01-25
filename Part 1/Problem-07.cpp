e2a26a8904cfe0553e6c031c55d6c8023607f507
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-01-25 10:06:38 +0600

    Added new solution.

---
+++ Part 1/Problem-07.cpp
@@ -0,0 +1,14 @@
+// Input two numbers and show their average.
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    float x, y;
+
+    cin >> x >> y;
+
+    cout << (x + y) / 2;
+}    
