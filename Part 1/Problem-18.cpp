c5b220ab7be92ac17343d567737d1baf1c14864b
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-01-25 10:55:07 +0600

    Added new solution.

---
+++ Part 1/Problem-18.cpp
@@ -0,0 +1,72 @@
+// Given three natural numbers a, b, c which represent the day, month and year of some date. Output “yes" if the given date is correct and “no” otherwise.
+
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+	int a, b, c;
+
+	cin >> a >> b >> c;
+
+	if (c > 0 && b > 0 && b < =12 && a > 0) {
+		if (((c % 100 != 0) && (c % 4 == 0)) || (c % 400 == 0)) {
+			if (b == 2) {
+				if (a <= 29 && a > 0) cout << "yes";
+				else cout << "no";
+			}
+			else {
+				if (b > 0 && b <= 12) {
+					if (b == 1 && a > 0 && a <= 31) cout << "yes";
+					else {
+						if (b == 3 && a > 0 && a <= 31) cout << "yes";
+						else goto loop;
+					}
+				}
+				else cout << "no";
+			}
+		}
+		else {
+			if (b == 1 && a > 0 && a <= 31) cout << "yes";
+			else {
+				if (b == 2 && a <= 28) cout << "yes";
+				else {
+					if (b == 3 && a > 0 && a <= 31) cout << "yes";
+					else {
+						loop:
+						if (b == 4 && a > 0 && a <= 30) cout << "yes";
+						else {
+							if (b == 5 && a > 0 && a <= 31) cout << "yes";
+							else {
+								if (b == 6 && a > 0 && a <= 30) cout << "yes";
+								else {
+									if (b == 7 && a > 0 && a <= 31) cout << "yes";
+									else {
+										if (b == 8 && a > 0 && a <= 31) cout << "yes";
+										else {
+											if (b == 9 && a > 0 && a <= 30) cout << "yes";
+											else {
+												if (b == 10 && a > 0 && a <= 31) cout << "yes";
+												else {
+													if (b == 11 && a > 0 && a <= 30) cout << "yes";
+													else {
+														if (b == 12 && a > 0 && a <= 31) cout << "yes";
+														else cout << "no";
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}	
+					}
+				}
+			}
+		}
+	}
+	else cout << "no";
+	
+	return 0;
+}
