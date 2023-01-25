324b5417d5a06d5efaf6c78e6ad30ff186b9512e
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-01-26 00:18:06 +0600

    Added new solution.

---
+++ Part 1/Problem-47.cpp
@@ -0,0 +1,30 @@
+/*
+  Rotate the matrix.
+
+  Your task is to rotate given matrix on 90 degree counterclockwise.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+    int n, m;
+    cin >> n >> m;
+  
+    int arr[n][m];
+  
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> arr[i][j];
+        }
+    }
+  
+    for (int i = m - 1; i >= 0; --i) {
+        for (int j = 0; j < n; ++j) {
+            cout << arr[j][i] << " ";
+        }
+        cout << endl;
+    }
+}
