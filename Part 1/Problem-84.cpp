3d0c6fc6931dceaf86783821f05d91e983345338
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-01-27 15:39:54 +0600

    Added new solution.

---
+++ Part 1/Problem-84.cpp
@@ -0,0 +1,34 @@
+/*
+  Write a function that have to copy one char array to other avoiding any non-letter symbols.
+
+  Note. Your program have to use pointer.
+*/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int func (char* text) 
+{
+	char res[101] = "";
+	int n = 0;
+
+	for (int i = 0; i < 101; i++)
+		if (isalpha(text[i])) {res[n] = text[i]; n++;}
+
+	cout << res;
+	
+	return 0;
+}
+
+int main () 
+{
+	char text[101] = "";
+
+	cin >> text;
+
+	func(text);
+
+	return 0;
+}
