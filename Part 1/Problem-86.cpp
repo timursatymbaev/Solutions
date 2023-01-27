374c86c11470237423ac2431b45f113068e4d0fc
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-01-27 15:40:13 +0600

    Added new solution.

---
+++ Part 1/Problem-86.cpp
@@ -0,0 +1,41 @@
+/*
+  Write a function that find the sum of positive elements in the array that are located in front of the maximum element.
+
+  Note. The program have to use pointer.
+*/
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int func (int* arr, int size) 
+{
+	int res = 0, io = 0, maxi;
+
+	maxi = *max_element(arr, arr + size);
+
+	for (int i = size - 1; i >= 0; i--) {
+		if (arr[i] == maxi) io = 1;
+
+		if (io == 1 && arr[i] > 0) res += arr[i];
+	}
+		
+	return res - maxi;
+}
+
+int main () 
+{
+	int size;
+
+	cin >> size;
+
+	int *arr = new int[size];
+
+	for (int i = 0; i < size; i++)
+		cin >> arr[i];
+
+	cout << func(arr, size);
+
+	return 0;
+}
