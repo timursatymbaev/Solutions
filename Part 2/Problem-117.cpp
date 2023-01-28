390e3b916f7e54e631433edc2d73577cc19fadee
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-01-28 14:34:57 +0600

    Added new solution.

---
+++ Part 2/Problem-117.cpp
@@ -0,0 +1,25 @@
+// Write a program that will replace all symbols from the given sequence in the given text by '*'.
+
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+
+int main () 
+{
+	int size;
+	string str, needFind;
+
+	getline(cin, str);
+	cin >> needFind;
+
+	size = needFind.length();
+
+	for (int index = 0; index < size; index++)
+		while (str.find(needFind[index]) != -1)
+			str.replace(str.find(needFind[index]), 1, 1, '*');
+
+	cout << str << endl;
+
+	return 0;
+}
