f61f3d734fe9efde39e01c977392e65359650bec
Timur <123528735+timursatymbaev@users.noreply.github.com>
2023-02-21 20:31:24 +0600

    Added 'Main.java' file.

---
+++ Assignment 4/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Coffee coffee = Coffee.getInstance(); // initialize Coffee class using Singleton
+
+        coffee.displayInterface(); // display user interface
+    }
+}
